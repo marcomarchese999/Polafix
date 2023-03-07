ae7b0c9d47ce3aa4826dbcbbfbbe18ee72ba448f
marcomarchese999 <marco.marchese5@studio.unibo.it>
2023-03-07 12:09:39 +0100

    PolafixSpringProject

---
+++ polafix/src/main/java/com/polafix/polafix/PolafixApplication.java
@@ -0,0 +1,13 @@
+package com.polafix.polafix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolafixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolafixApplication.class, args);
+	}
+
+}
---
+++ polafix/src/test/java/com/polafix/polafix/PolafixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polafix.polafix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolafixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
